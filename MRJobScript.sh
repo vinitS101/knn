b32d09bc22b67b7be5b27f02083a6cb3f10a0a40
Vinit Singh <vinit.singh9013@gmail.com>
2018-11-14 21:51:34 +0530

    Shell script to run the Job

---
+++ MRJobScript.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+export HADOOP_CLASSPATH=$(hadoop classpath)
+
+#Create Output directory
+hadoop fs -mkdir /Output
+
+#Changing directory to location with the Java program and the classes folder.
+cd Desktop/poker
+
+javac -classpath ${HADOOP_CLASSPATH} -d "classes" "KnnPokerhand.java"
+jar -cvf KNN.jar -C classes/ .
+
+#Finally run the job. Locations are locations of the training and testing data set in HDFS
+hadoop jar KNN.jar KnnPokerhand /training_data/train.txt /Output/out /testing_data/test.txt
+
+#Creating txt file with the result for accuracy testing
+hadoop fs -cat /Output/out_*/part-r-00000 > KNNOutput.txt
