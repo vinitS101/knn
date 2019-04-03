9fca9b558538cd5f3858364e36c4e4674c9a3109
vinitS101 <vinit.singh9013@gmail.com>
2019-04-03 22:58:56 +0530

    Added some comments, cleaned up.

--- SparkAppCode/KnnPokerhand.scala
+++ SparkAppCode/KnnPokerhand.scala
@@ -8,11 +8,13 @@ import java.io.PrintWriter
 
 object KnnPokerhand {
   
-  //Change value of K as needed (Can also been passed as an argument)
+  // Change value of K as needed (Can also been passed as an argument)
   val K = 8;
-  //Stores value of Current Test Case
+
+  // Stores value of Current Test Case
   var testData = new Array[Double](10)
-  //Range of values for suits and ranks
+
+  // Range of values for suits and ranks
   val minSuit: Double = 1.0
   val maxSuit: Double = 4.0
   val minRank: Double = 1.0
@@ -23,13 +25,14 @@ object KnnPokerhand {
     return ((givenVal - minVal) / (maxVal - minVal));
   }
   
-  // Takes a double and returns its squared value.
+  // Takes a double and returns it's squared value.
   def squaredDistance(givenVal: Double) : Double = {
     return (givenVal*givenVal);
   }
   
-  // Takes ten pairs of values (three pairs of doubles and two of strings), finds the difference between the members
-  // of each pair (using nominalDistance() for strings) and returns the sum of the squared differences as a double.
+  // Takes ten pairs of values (three pairs of doubles and two of strings),
+  // finds the difference between the members of each pair
+  // and returns the sum of the squared differences as a double.
   def totalSquaredDistance(trainData: Array[Double]) : Double = {
 
     var diffArr: Double = 0.0
@@ -40,28 +43,30 @@ object KnnPokerhand {
     return (diffArr)
   }
 
-  // =================
-  //  Mapper Function
-  // =================
+  // Mapper Function
+
   def theMapper(line: String) = {
     var trainData: Array[Double] = new Array[Double](11)
 
     // Split the input line
     val fields = line.split(",")
-    //Array to store Suits and Ranks of Current Training Data 
+
+    // Array to store Suits and Ranks of Current Training Data 
     for(i <- 0 to 9)
       trainData(i) = normalisedDouble(fields(i).toDouble, minSuit, maxSuit)
+
     // PokerClass
     val pClass = fields(10).toInt
-    //Calculate and store the Euclidian distance of current test case from current training case
+
+    // Calculate and store the Euclidian distance of current test case from current training case
     val tDist = totalSquaredDistance(trainData)
-    //Pass the distance and class as a tuple
+
+    // Pass the distance and class as a tuple
     (tDist, pClass)
   }
   
-  // ==========================================================================================
-  //                      Main Function
-  // ==========================================================================================
+  // Main Function
+
   def main(args: Array[String]) {
    
     // Set the log level to only print errors
@@ -73,17 +78,11 @@ object KnnPokerhand {
     val sc = new SparkContext(conf)
 
     // Load each line of the source data into an RDD
-    println("===================================================")
-    println("\nLoading Training Data...")
-    val lines = sc.textFile("s3://.../TrainDataFinal.txt") //Fix location of File as needed
+    val lines = sc.textFile("s3://.../TrainDataFinal.txt") // Fix location of File as needed
     
-    //Working with testFile
-    println("===================================================")
-    println("\nLoading Testing Data...")
-    val testLines = (sc.textFile("s3://.../TestDataFinal.txt")).collect //Fix location of File as needed
+    // Working with testFile
+    val testLines = (sc.textFile("s3://.../TestDataFinal.txt")).collect // Fix location of File as needed
 
-    println("===================================================")
-    println("Writing to File now...")
     val writer = new PrintWriter(new File("write.txt"))
     
     for (testLine <- testLines)
@@ -95,28 +94,29 @@ object KnnPokerhand {
         
         // Use our theMapper function to convert to (Distance, PokerClass) tuples
         val rdd = lines.map(theMapper)
-        //Sort the rdd elements in an ascending order
+
+        // Sort the rdd elements in an ascending order
         val sortedRdd = rdd.sortByKey()
-        // Finally take and store top K elements in an array.
+
+        // Take and store top K elements in an array.
         val kNearestNeighbors = sortedRdd.take(K)
         
-        //New array
         var classArr = new Array[Int](K)
         
-        //Store the classes in an Array
+        // Store the classes in an Array
         for(i <- 0 to (K-1))
           classArr(i) = kNearestNeighbors(i)._2
           
-        //Sort and store array of classes 
+        // Sort and store array of classes 
         val newArr = classArr.sorted
       
-      //Pre-select the class at index 0
+        // Pre-select the class at index 0
         var mostCommonClass = newArr(0)
         var freq = 1
         var currFreq = 1
         var currClass = newArr(0)
         
-        //Check for class with highest frequency
+        // Check for class with highest frequency
         for(i <- 1 to (K-1)) {
            if(currClass == newArr(i)) {
              currFreq = currFreq + 1
@@ -131,16 +131,12 @@ object KnnPokerhand {
            }
         }
         
-        if(freq < currFreq) {
+        if ( freq < currFreq ) {
           mostCommonClass = currClass
           freq = currFreq
         }
-        
-        println("===================================================")
-        println("     Writing Next Case:")
 
         writer.write(mostCommonClass + ",")
-        println("Done!")
      }
 
     writer.close()
